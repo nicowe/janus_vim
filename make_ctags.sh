62622e6fb73851548b2d5cfa919c71f12f52e931
Nico Weinert <nico.weinert@googlemail.com>
2013-11-09 22:52:59 +0000

    Added YouCompleteMe

--- make_ctags.sh
+++ make_ctags.sh
@@ -1,1 +1,1 @@
-ctags -R --excmd=number --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.janus/tags/ctags /usr/include
+ctags -R --excmd=number --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.janus/tags/ctags /usr/include/c++
